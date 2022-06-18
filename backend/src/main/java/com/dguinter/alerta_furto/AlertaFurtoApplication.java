84d12eba97621d5a0c02d2bba3c5ed81150c1430
Daniel <guinterdaniel@gmail.com>
2022-06-18 01:24:16 -0300

    Project created

---
+++ backend/src/main/java/com/dguinter/alerta_furto/AlertaFurtoApplication.java
@@ -0,0 +1,13 @@
+package com.dguinter.alerta_furto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaFurtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaFurtoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dguinter/alerta_furto/AlertaFurtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dguinter.alerta_furto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaFurtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
