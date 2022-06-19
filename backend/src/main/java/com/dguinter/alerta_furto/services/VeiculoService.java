cb337e118de788e13f04157ae9aad524cb49f7d1
Daniel <guinterdaniel@gmail.com>
2022-06-19 10:45:05 -0300

    Data base seeding

--- backend/src/main/java/com/dguinter/alerta_furto/services/VeiculoService.java
+++ backend/src/main/java/com/dguinter/alerta_furto/services/VeiculoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dguinter.alerta_furto.entities.Veiculo;
 import com.dguinter.alerta_furto.repositories.VeiculoRepository;
@@ -13,6 +14,8 @@ public class VeiculoService {
 
 	@Autowired
 	private VeiculoRepository repository;
+	
+	@Transactional(readOnly = true)
 	public List<Veiculo> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_veiculo(name) VALUES ('sedan');
+INSERT INTO tb_veiculo(name) VALUES ('hatch');
+INSERT INTO tb_veiculo(name) VALUES ('SUV');
+INSERT INTO tb_veiculo(name) VALUES ('minivan');
+INSERT INTO tb_veiculo(name) VALUES ('van');
+INSERT INTO tb_veiculo(name) VALUES ('caminhonete');
+INSERT INTO tb_veiculo(name) VALUES ('caminhão');
