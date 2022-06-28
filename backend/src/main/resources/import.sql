4178354e8efbc518252370c63bde6ee6b060f512
Daniel <guinterdaniel@gmail.com>
2022-06-27 22:44:05 -0300

    Data auditing

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO tb_category(name) VALUES ('sedan');
-INSERT INTO tb_category(name) VALUES ('hatch');
-INSERT INTO tb_category(name) VALUES ('SUV');
-INSERT INTO tb_category(name) VALUES ('minivan');
-INSERT INTO tb_category(name) VALUES ('van');
-INSERT INTO tb_category(name) VALUES ('caminhonete');
-INSERT INTO tb_category(name) VALUES ('caminhão');
+INSERT INTO tb_category(name, created_At) VALUES ('sedan', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('hatch', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('SUV', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('minivan', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('van', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('caminhonete', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('caminhão', NOW());
